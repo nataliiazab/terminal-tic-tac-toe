024e01afccae3efa6f9ea9a92104eea92aad6a9b
AndrianaOS <andrianasaffo@gmail.com>
2022-12-11 15:16:56 +0000

    Initialisation of board -Incomplete

--- board-printer.js
+++ board-printer.js
@@ -15,10 +15,26 @@
         =================
     Test your function by calling it with an example tic-tac-toe board.
 */
-export function printBoard(board) {
-  hi;
-}
 
+let emptyBoard = [
+  ["X", "_", "_"],
+  ["_", "X", "_"],
+  ["O", "O", "X"],
+];
+
+// for (let i = 0; i < emptyBoard.length; i++) {
+//   let row = emptyBoard[i];
+//   console.log(row);
+//}
+
+export function printBoard(board) {
+  for (let i = 0; i < emptyBoard.length; i++) {
+    let row = emptyBoard[i];
+    console.log(row);
+  }
+}
+//emptyBoard.forEach((row) => console.log(row));
+printBoard();
 /*
     Given a tic-tac-toe board (an array of arrays),
         - return true if there are no moves left to make (there are no more '_' values)
